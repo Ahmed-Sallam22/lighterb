bd4ce5f4edbb2de4a52620818632a813acd45c66
Peter Joseph <73145539+PeterTorki@users.noreply.github.com>
2025-12-09 13:41:58 +0200

    Add HeroPattern component with SVG graphics

---
+++ src/ui/HeroPatterns.jsx
@@ -0,0 +1,77 @@
+const HeroPattern = () => (
+	<svg
+		className="pointer-events-none absolute  "
+		width="871"
+		height="322"
+		viewBox="0 0 871 322"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<g opacity="0.1">
+			<mask id="mask0_177_13244" maskUnits="userSpaceOnUse" x="0" y="0" width="871" height="308">
+				<rect x="0.570312" y="0.219971" width="870.316" height="306.873" rx="40" fill="#D3D3D3" />
+			</mask>
+			<g mask="url(#mask0_177_13244)">
+				<g opacity="0.5">
+					<path
+						d="M18.3834 263.045L52.9843 273.755L-16.53 296.535L18.3834 263.045Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M3.95954 -27.7887L38.5604 -17.079L-30.9538 5.70101L3.95954 -27.7887Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M-10.5961 206.382L27.6497 199.483L-26.3255 249.003L-10.5961 206.382Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M113.076 196.198L116.141 229.722L65.3326 180.103L113.076 196.198Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M-8.28617 127.401L-37.0226 167.353L-36.158 70.0641L-8.28617 127.401Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M290.992 284.036L294.057 317.561L243.249 267.942L290.992 284.036Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M36.124 233.656L36.125 233.656L55.6962 214.878L55.5298 259.447L20.53 248.614L36.124 233.656Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M28.9447 152.099L54.1792 163.039L7.98389 176.879L8.20654 143.105L28.9447 152.099Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M202.35 287.603L227.584 298.543L181.389 312.383L181.612 278.609L202.35 287.603Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M138.982 252.033L166.036 244.613L141.805 283.258L117.246 257.992L138.982 252.033Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+					<path
+						d="M94.0883 317.309L70.3921 276.017L107.302 274.416L94.0883 317.309Z"
+						stroke="#28819C"
+						stroke-width="3"
+					/>
+				</g>
+			</g>
+		</g>
+	</svg>
+);
+
+export default HeroPattern;
