a5b74aba813be528c9d23b2a4a4c53668dc4b2ec
Ahmed-Sallam22 <ahmed30sallam22@gmail.com>
2025-12-12 15:42:33 +0200

    update lang

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,3 @@
 // import logoPath from "../assets/logo.png";
-// export const LANGUAGE_STORAGE_KEY = "lighterp_locale";
+export const LANGUAGE_STORAGE_KEY = "lighterp_locale";
 // export default logoPath;
